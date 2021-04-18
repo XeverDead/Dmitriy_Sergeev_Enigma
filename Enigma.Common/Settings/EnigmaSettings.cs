28e2b18af517a6f9bc10dd88732a6f801b5654a6
XeverDead <xever.dead.115@gmail.com>
2021-04-18 15:23:20 +0300

    Settings update

--- Enigma.Common/Settings/EnigmaSettings.cs
+++ Enigma.Common/Settings/EnigmaSettings.cs
@@ -2,7 +2,7 @@
 {
     public static class EnigmaSettings
     {
-        public const string MainDirectory = "D:\\web";
+        public const string MainDirectory = "web";
         public static readonly string UserListPath = MainDirectory + "\\UserList.eul";
         public const string MessageExtension = "ems";
         public const string UserDataFileName = "data.eud";
